1e4bb91edf39d7dda24a65d6e896ac2bf2926826
xiexiaohao <2725642450@qq.com>
2021-12-27 21:46:06 +0800

    改变properties

--- src/main/java/com/example/androidproject/AndroidProjectApplication.java
+++ src/main/java/com/example/androidproject/AndroidProjectApplication.java
@@ -3,14 +3,21 @@ package com.example.androidproject;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 //@MapperScan("com.example.androidproject.dao")
-public class AndroidProjectApplication {
+public class AndroidProjectApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(AndroidProjectApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(AndroidProjectApplication.class);
     }
 
 }
